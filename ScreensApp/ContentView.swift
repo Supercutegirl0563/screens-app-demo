4df2cc494fe42de6c8a7d016c56d8915eedaedc8
Alvin Phyo Htet <alvinpt100@gmail.com>
2026-07-04 14:46:31 +0800

    added name and background color

--- ScreensApp/ContentView.swift
+++ ScreensApp/ContentView.swift
@@ -9,13 +9,21 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        ZStack {
+            Color.purple
+            
+            
+            VStack {
+                Text("Alvin")
+                    .font(.largeTitle)
+            }
+            
+            
+            
         }
-        .padding()
+        .ignoresSafeArea()
+        
     }
 }
 
